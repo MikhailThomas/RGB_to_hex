a10a41b2b9a536190150d8e32d5db25d024ab4e4
mikhail <mikhailthomas29@gmail.com>
2022-06-20 14:58:05 +0200

    .

---
+++ script/code.js
@@ -0,0 +1,9 @@
+function ColorToHex(color) {
+    let hexadecimal = color.toString(16);
+    return hexadecimal.length == 1 ? "0" + hexadecimal : hexadecimal;
+  }
+  
+  function ConvertRGBtoHex(red, green, blue) {
+    return "#" + ColorToHex(red) + ColorToHex(green) + ColorToHex(blue);
+  }
+  console.log(ConvertRGBtoHex(260, 80, 220));
